2fb6bc23a35b9c9cea5546fa942b2d8ff4e672b6
Natalie Rote <rote.n@stu.janesville.k12.wi.us>
2025-04-11 14:33:13 -0500

    4/11/25

---
+++ Langdocs/prog54d.js
@@ -0,0 +1,7 @@
+function doMath() {
+let bse = Number(document.getElementById('bse').value);
+    let hght = Number(document.getElementById('hght').value);
+    let area = Base * Height / 2;
+    let perim = 2 * length + 2 * width;
+    document.writeln('Area: ' + area + ' Perimiter: ' + perim);
+}
