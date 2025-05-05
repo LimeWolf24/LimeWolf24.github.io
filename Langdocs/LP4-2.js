ecdba0e88a205cfadad4c1402d1d8c929e7acafa
Natalie Rote <rote.n@stu.janesville.k12.wi.us>
2025-05-05 14:35:47 -0500

    5/5/25

---
+++ Langdocs/LP4-2.js
@@ -0,0 +1,11 @@
+       function doMath() {
+       if wght > 27 {
+            alert('Too heavy.')
+        }
+        if hght > 0.1 cubic meters {
+            alert('Too large.')
+        }
+        if wght > 27 and hght > 0.1 cubic meters {
+            alert('Too heavy and too large.')
+        }
+    }
